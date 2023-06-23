2c708019049131f6862ce2611db7722d4086d475
Solovev Anton <solovev.anton93@gmail.com>
2023-06-23 14:03:01 +0300

    added UI main form and controller

--- src/main/java/com/solovev/quiz_game/controllers/MainController.java
+++ src/main/java/com/solovev/quiz_game/controllers/MainController.java
@@ -1,4 +1,14 @@
 package com.solovev.quiz_game.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+
 public class MainController {
+    public CheckBox checkBoxCorrectAnswers;
+
+    public void buttonLoadFromInternet(ActionEvent actionEvent) {
+    }
+
+    public void buttonLoadFromFile(ActionEvent actionEvent) {
+    }
 }
