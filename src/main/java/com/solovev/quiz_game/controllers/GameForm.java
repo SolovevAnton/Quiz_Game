e9d4fd8ce08b7d2a88ba14519239504a0a6f0e64
Solovev Anton <solovev.anton93@gmail.com>
2023-08-08 18:39:20 +0200

    updated game form

--- src/main/java/com/solovev/quiz_game/controllers/GameForm.java
+++ src/main/java/com/solovev/quiz_game/controllers/GameForm.java
@@ -1,4 +1,14 @@
 package com.solovev.quiz_game.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+
 public class GameForm {
+    public TextField textFieldCorrectAnswers;
+    public TextField textFieldCorrectRate;
+    public TabPane tabPainMain;
+
+    public void checkResults(ActionEvent actionEvent) {
+    }
 }
